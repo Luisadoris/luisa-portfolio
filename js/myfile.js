530fcd103ba9972386412882aa29cd4c0296d1e3
Luisadoris <luisa.fernandez@stu.bmcc.cuny.edu>
2021-10-14 20:21:19 -0400

    commit

---
+++ js/myfile.js
@@ -0,0 +1,12 @@
+
+<script>
+<!-- JAVSCRIPT NAV -->
+    function myFunction() {
+      var x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+    </script>
